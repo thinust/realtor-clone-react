9f7375d2ebf9c8f40abd586e85cc98c85469806b
thinust <Muhandiramge.Wijegunawardena@mail.bcu.ac.uk>
2024-03-31 06:09:08 +0530

    add the edit functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
-import {getAuth} from "firebase/auth";
+import {getAuth, updateProfile} from "firebase/auth";
 import {useNavigate} from "react-router-dom";
+import {doc, updateDoc} from "firebase/firestore";
+import {db} from "../firebase";
+import {toast} from "react-toastify";
 
 export default function Profile() {
 
     const auth = getAuth();
     const navigate = useNavigate();
+    const [changeDetail, setChangeDetail] = useState(false);
     const [formData, setFormData] = useState({
         name: auth.currentUser.displayName,
         email: auth.currentUser.email,
@@ -18,6 +22,38 @@ export default function Profile() {
         navigate('/');
     }
 
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }));
+    }
+
+    async function onSubmit() {
+
+        try {
+            if (auth.currentUser.displayName !== name) {
+
+                //Update display name in firebase authentication
+                await updateProfile(auth.currentUser, {
+                    displayName: name,
+                });
+
+                // update name in the fires store
+
+                const docRef = doc(db, "users", auth.currentUser.uid);
+                await updateDoc(docRef, {
+                        name,
+                    }
+                );
+
+            }
+            toast.success("Profile name Updated Successfully");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -25,8 +61,9 @@ export default function Profile() {
                 <div className="w-full md:w-[50%] mt-6 px-3">
                     <form>
                         {/*Name Input*/}
-                        <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300
-                        rounded transition ease-in-out"/>
+                        <input type="text" id="name" value={name} disabled={!changeDetail} onChange={onChange}
+                               className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300
+                        rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}/>
 
                         {/*Email Input*/}
                         <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300
@@ -34,12 +71,15 @@ export default function Profile() {
 
                         <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
                             <p className="flex items-center">Do you want change your name?
-                                <span
-                                    className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
+                                <span onClick={() => {
+                                    changeDetail && onSubmit()
+                                    setChangeDetail((prev) => !prev)
+                                }}
+                                      className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">{changeDetail ? "Apply Change" : "Edit"}</span>
                             </p>
                             <p onClick={onLogOut}
-                               className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer">Sign
-                                Out</p>
+                               className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer">
+                                Sign Out</p>
                         </div>
                     </form>
                 </div>
